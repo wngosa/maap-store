74346bac06ec6b4db4982f0229146b0fdf7ffe7a
Martin Verzilli <mverzilli@manas.tech>
2019-08-09 12:41:57 -0300

    Freeze level&teaching options (Rubocop suggestion)

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,7 +1,7 @@
 class Site < ApplicationRecord
   OWNERSHIPS = %w[public private mission military other].freeze
-  LEVEL = %w[primary secondary tertiary unknown]
-  TEACHING = %w[teaching non_teaching unknown]
+  LEVEL = %w[primary secondary tertiary unknown].freeze
+  TEACHING = %w[teaching non_teaching unknown].freeze
 
   validates :name, :address, :location, presence: true
 end
