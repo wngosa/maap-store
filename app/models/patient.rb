5910dfad4f56d3d98399ff87abb0e314b9d1ef12
hdf1986 <hugo.farji+github@gmail.com>
2019-08-09 17:18:53 -0300

    Added relations for patient & sites

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,4 +2,5 @@ class Patient < ApplicationRecord
   include PatientIdObfuscation
 
   validates :patient_id, presence: true, uniqueness: { scope: :site_id }
+  belongs_to :site
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -2,4 +2,6 @@ class Site < ApplicationRecord
   OWNERSHIPS = %w[public private].freeze
 
   validates :name, :address, :location, presence: true
+  has_many :patient_id_hashes, dependent: :destroy
+  has_many :patients, dependent: :destroy
 end
