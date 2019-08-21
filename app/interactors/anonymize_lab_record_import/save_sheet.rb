57e6528653e73e8486f76079f1b9ec1893976fda
hdf1986 <hugo.farji+github@gmail.com>
2019-08-21 13:22:40 -0300

    Add optimization on obfuscation worker & fixed extensions

--- app/interactors/anonymize_lab_record_import/save_sheet.rb
+++ app/interactors/anonymize_lab_record_import/save_sheet.rb
@@ -6,7 +6,7 @@ module AnonymizeLabRecordImport
       context.sheet_file.write("#{context.sheet_path}.xlsx")
       context.lab_record.sheet_file.attach(
         io: File.open("#{context.sheet_path}.xlsx"),
-        filename: 'Download.xlsx'
+        filename: "Download.#{context.sheet_type == :xlsx ? 'xlsx' : 'xls'}"
       )
       context.lab_record.save!
     end
