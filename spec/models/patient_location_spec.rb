a79d8cea95db63363c76ce7abd2e3b3929806186
Martin Verzilli <mverzilli@manas.tech>
2019-08-21 15:35:09 -0300

    Fix Rubocop offenses

--- spec/models/patient_location_spec.rb
+++ spec/models/patient_location_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
- RSpec.describe PatientLocation, type: :model do
+RSpec.describe PatientLocation, type: :model do
   subject { build :patient_location }
 
-   it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:name) }
 
-   it { is_expected.to be_valid }
+  it { is_expected.to be_valid }
 end
