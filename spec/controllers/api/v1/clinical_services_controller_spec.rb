e21a60804b507de9869adb65e9de9f3adabec906
hdf1986 <hugo.farji+github@gmail.com>
2019-08-21 11:04:36 -0300

    Fix rubocop indentation

--- spec/controllers/api/v1/clinical_services_controller_spec.rb
+++ spec/controllers/api/v1/clinical_services_controller_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
- RSpec.describe Api::V1::ClinicalServicesController, type: :controller do
+RSpec.describe Api::V1::ClinicalServicesController, type: :controller do
   describe 'GET /clinical_services' do
     context 'with an unauthenticated user' do
       before { get :index }
 
-       it 'should respond unauthorized' do
+      it 'should respond unauthorized' do
         expect(response).to have_http_status(:unauthorized)
       end
     end
 
-     context 'with an authenticated user' do
+    context 'with an authenticated user' do
       let(:user) { create :user }
       let!(:clinical_services) { create_list :clinical_service, 10 }
       before do
@@ -18,11 +18,11 @@ require 'rails_helper'
         get :index
       end
 
-       it 'responds ok' do
+      it 'responds ok' do
         expect(response).to have_http_status(:ok)
       end
 
-       it_behaves_like 'a paginated request', ClinicalService.all
+      it_behaves_like 'a paginated request', ClinicalService.all
     end
   end
 end
