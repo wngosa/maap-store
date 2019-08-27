1fa2875716e0ce4a328337b58e93d0432fc1c387
hdf1986 <hugo.farji+github@gmail.com>
2019-08-27 10:30:57 -0300

    Fix for specs

--- spec/factories/sites.rb
+++ spec/factories/sites.rb
@@ -4,12 +4,8 @@ FactoryBot.define do
     address { FFaker::Address.street_address }
     location { "#{FFaker::Geolocation.lat}, #{FFaker::Geolocation.lng}" }
     ownership { Site::OWNERSHIPS.sample }
-    level { Site::LEVEL.sample }
-    teaching { Site::TEACHING.sample }
     has_farmacy { FFaker::Boolean.maybe }
     has_hospital { FFaker::Boolean.maybe }
     has_laboratory { FFaker::Boolean.maybe }
-    identified_patients { FFaker::Boolean.maybe }
-    permanently_identified_patients { FFaker::Boolean.maybe }
   end
 end
