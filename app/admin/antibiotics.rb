e6e42d4630fd1d208f85ba91c843862fe74e8bd5
hdf1986 <hugo.farji+github@gmail.com>
2019-09-02 11:54:59 -0300

    Updated filters

--- app/admin/antibiotics.rb
+++ app/admin/antibiotics.rb
@@ -1,7 +1,11 @@
 ActiveAdmin.register Antibiotic do
+  preserve_default_filters!
+  remove_filter :antibiotic_consumption_stats
+
   actions :index, :show, :new, :create, :edit, :update
   permit_params :name, :strength_value, :strength_unit, :form, :pack_size, 
                 :brand
+
   form do |f|
     inputs 'Details' do
       input :name
