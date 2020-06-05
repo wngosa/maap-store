5b7757df4e18d44fe500d2757f9bde98bfd1a917
Leandro Matayoshi <lmatayoshi@manas.com.ar>
2020-06-05 15:11:59 -0300

    For #308. Hash patient when creating it
    Instead of triggering when importing lab records

--- app/interactors/interactors_lri/hash_patients_in_records.rb
+++ app/interactors/interactors_lri/hash_patients_in_records.rb
@@ -2,36 +2,18 @@ module InteractorsLRI
   class HashPatientsInRecords
     include Interactor
 
-    def call # rubocop:disable Metrics/AbcSize
+    def call
       Rails.logger.info 'Patients hashing in Lab Records started'
-      obfuscate_patients
       lab_record_import = context.record
       lab_record_import.lab_records.obfuscation_pending.find_each(batch_size: 500) do |lr|
-        lr.patient_id = patient_id_hash_for(lr.patient_id, lr.site_id) if lr.patient_id.present?
+        if lr.patient_id.present?
+          lr.patient_id =
+            Patient.patient_id_hash_for(lr.patient_id, lr.site_id)
+        end
         lr.patient_id_state = 'obfuscated'
         lr.save!
       end
       Rails.logger.info 'Patients hashing in Lab Records finished'
-    end
-
-    private
-
-    def patient_id_hash_for(patient_id, site_id)
-      PatientIdHash.find_or_initialize_by(
-        patient_id: patient_id, site_id: site_id
-      ) do |patient_id_hash|
-        patient_id_hash.hashed_value = SecureRandom.uuid
-        patient_id_hash.save
-      end.hashed_value
-    end
-
-    def obfuscate_patients
-      Patient.obfuscation_pending.find_each do |patient|
-        Rails.logger.info "Obfuscating patient#{patient.id}"
-        patient.patient_id = patient_id_hash_for(patient.patient_id, patient.site_id)
-        patient.patient_id_state = 'obfuscated'
-        patient.save!
-      end
     end
   end
 end
