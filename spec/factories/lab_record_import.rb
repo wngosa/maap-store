ddb3d7eb4eefe50b1503ec09ba0caecae36cb6b7
Leandro Matayoshi <lmatayoshi@manas.com.ar>
2020-08-01 20:48:22 -0300

    Add LabRecordImport model specs

--- spec/factories/lab_record_import.rb
+++ spec/factories/lab_record_import.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :lab_record_import do
+    header_row { 1 }
+    data_rows_from { 2 }
+    data_rows_to { 6 }
+    patient_or_lab_record_id { "[\"patientId\",\"labRecordId\",null,null,null]" }
+    phi { "[false,false,true,true,false]" }
     site
   end
 end
